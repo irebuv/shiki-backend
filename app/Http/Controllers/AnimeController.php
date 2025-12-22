65f8ade2ab3777c7db4adaa1b4784ebb5ee4b71e
irebuv <irebuv671@gmail.com>
2025-12-22 19:17:44 +0200

    sortBy

--- app/Http/Controllers/AnimeController.php
+++ app/Http/Controllers/AnimeController.php
@@ -7,10 +7,24 @@ use Illuminate\Http\Request;
 
 class AnimeController extends Controller
 {
-    public function index(Request $request){
+    public function index(Request $request)
+    {
 
+        $sort       = trim((string) $request->input('sort', 'id'));
+        $split = explode(':', $sort);
         $query = Anime::query();
+        if ($split[0] === 'random') {
+            $daySeed = now()->timezone('Europe/Kyiv')->format('Y-m-d');
+            $seed = hash('sha256', $daySeed . config('app.key'));
+            $query->orderByRaw('CRC32(CONCAT(?, anime.id))', [$seed]);
+        } else {
+            $direction = $split[1];
+            $sortBy = $split[0];
+            $query->orderBy($sortBy, $direction);
+        }
+
         $perPage = 12;
+        logger($split);
         $paginator = $query->paginate($perPage)->appends($request->query());
         $items = $paginator->items();
         return response()->json([
@@ -24,6 +38,10 @@ class AnimeController extends Controller
                 'next_page_url' => $paginator->nextPageUrl(),
                 'prev_page_url' => $paginator->previousPageUrl(),
                 'has_more'      => $paginator->hasMorePages(),
+            ],
+
+            'filters' => [
+                'sort' => $sort,
             ],
         ]);
     }
